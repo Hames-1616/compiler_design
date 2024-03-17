5dd4f6c1a3dd11ab979511336b17706c65e8546d
Hames-1616 <bilquis748@gmail.com>
2024-03-17 22:19:51 +0530

    📦 NEW: validation

--- token_validation.c
+++ token_validation.c
@@ -1,42 +1,48 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-void main()
-{
-    char s[100];
-    gets(s);
+#include <stdio.h>
+#include <ctype.h>
 
-    char token[100];
+int main() {
+    char grammar[10];
+    char tok[10];
+    char tokens[10];
 
-    for(int i = 0 ; i < strlen(s) ; i++)
-    {
-        if(s[i] == '{' || s[i] == '(')
-        {
-            // printf("4");
-            token[i] = '4';
+    printf("Enter the grammar of your language: ");
+    scanf("%s", grammar);
+
+    int i = 0;
+    while (grammar[i] != '\0') {
+        if (grammar[i] == '{' || grammar[i] == '(') {
+            tok[i] = '4';
+        } else if (grammar[i] == '}' || grammar[i] == ')') {
+            tok[i] = '5';
+        } else if (grammar[i] == '+') {
+            tok[i] = '2';
+        } else if (grammar[i] == '*') {
+            tok[i] = '3';
+        } else if (isdigit(grammar[i])) {
+            tok[i] = '6';
         }
-        else if(isdigit(s[i]))
-        {
-            
-            // printf("6");
-            token[i] = '6';
-        }
-        else if(s[i] == '+')
-        {
-            // printf("2");
-            token[i] = '2';
-        }
-        else if(s[i] == '*')
-        {
-            // printf("3");
-            token[i] = '3';
-        }
-        else if(s[i] == '}' || s[i] == ')')
-        {
-            // printf("5");
-            token[i] = '5';
+        i++;
+    }
+    tok[i] = '\0';
+
+    printf("Original grammar: %s\n", grammar);
+    printf("Modified grammar: %s\n", tok);
+
+    int size = i;
+    i = 0;
+    while (i < size) {
+        if ((tok[i] == '6' && tok[i + 1] == '2' && tok[i + 2] == '6') || (tok[i] == '6' && tok[i + 1] == '3' && tok[i + 2] == '6') ||
+            (tok[i] == '4' && tok[i + 1] == '6' && tok[i + 2] == '5')) {
+            tokens[i] = '6';
+            i += 3;
+        } else {
+            tokens[i] = tok[i];
+            i++;
         }
     }
-    printf("%s",token);
+    tokens[i] = '\0';
+    printf("Modified grammar: %s\n", tokens);
 
+    return 0;
 }
